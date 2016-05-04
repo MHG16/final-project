e7c64c20e07505c5321080c67ad9062091f295a6
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-04 11:03:48 -0500

    added rebel to user migration  and created planets migration

--- migrations/20160503213152_create_planets.js
+++ migrations/20160503213152_create_planets.js
@@ -1,17 +1,17 @@
+
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('users', function(t) {
-		t.increments('id').unsigned().primary();
+  		t.increments('id').unsigned().primary();
 		t.dateTime('createdAt').notNull();
 		t.dateTime('updatedAt').nullable();
 		t.dateTime('deletedAt').nullable();
 
-		t.string('firstName').nullable();
-		t.string('lastName').nullable();
-		t.string('email').nullable();
-		t.boolean('rebel').nullable();
-	});
+		t.string('planetName').nullable();
+		t.string('image').nullable();
+
+   });
 };
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTable('users');
+  
 };
