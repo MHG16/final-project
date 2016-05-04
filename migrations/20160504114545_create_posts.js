02e4b8f65c12fd634fb8073e470daf197520866e
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-04 12:05:31 -0500

    finished create_posts migration

---
+++ migrations/20160504114545_create_posts.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('posts', function(t) {
+  		t.increments('id').unsigned().primary();
+		t.dateTime('createdAt').notNull();
+		t.dateTime('updatedAt').nullable();
+		t.dateTime('deletedAt').nullable();
+
+		t.text('body').notNull();
+		t.integer('planetId')
+			.unsigned()
+			.notNull()
+			.references('id')
+			.inTable('planets')
+			.onDelete('CASCADE');
+
+		t.integer('userId')
+			.unsigned()
+			.notNull()
+			.referenes('id')
+			.inTable('users')
+			.onDelete('CASCADE');
+
+   });
+  
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('posts');
+};
