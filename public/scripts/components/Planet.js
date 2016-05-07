9f793ea5eebe9a8c97c711e040e9bc92868e6f9f
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-07 13:14:51 -0500

    added / to the route for the Planet page

--- public/scripts/components/Planet.js
+++ public/scripts/components/Planet.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 export default React.createClass({
 	render: function() {
+		console.log(this.props.id);
 		return (
 				<section>
-					<a href src="#" >{this.props.name}</a>
+					<a href src="http://localhost:3000/planet/ + {this.props.id}">{this.props.name}</a>
 				</section>
 				);
 	}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -14,7 +14,7 @@ const router = (
 	<Router history={browserHistory}>
 		<Route path="/" component={App}>
 			<IndexRoute component={HomeGalaxy}/>
-			<Route path="/planet:planetId" component={PlanetPage}/>
+			<Route path="/planet/:planetId" component={PlanetPage}/>
 		</Route>
 	</Router>
 );
