01354271293d04b09c5f74622ffa328c274fdf83
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-07 13:28:02 -0500

    added necessary code to get router working

--- public/scripts/components/pages/PlanetPage.js
+++ public/scripts/components/pages/PlanetPage.js
@@ -12,45 +12,6 @@ export default React.createClass({
 });
 
 
-// import React from 'react';
-// import planetcollection from './collections/PlanetCollection';
-// import Planet from './Planet.js';
-
-
-// const Planet = React.createClass ({
-// 	getInitialState: function(){
-// 		console.log('getInitialState');
-// 		return{planetcollection: planetcollection};
-// 	},
-// 	componentDidMount: function(){
-// 		console.log('I mounted Planet!');
-// 		this.state.planetcollection.fetch();
-// 		this.state.planetcollection.on('update', () => {
-// 			this.setState({planetcollection: planetcollection});
-// 			console.log('testing');
-// 		});
-	
-// 	},
-
-// 	render: function(){
-// 			let planets = this.state.planetcollection.map(
-// 				function(value, index, array) {
-// 					return (<Planet
-// 					key = {value.get('id')}
-// 					id = {value.get('id')}
-// 					name={value.get('name')}
-// 					climate = {value.get('climate')}
-// 					diameter = {value.get('diameter')}
-// 					population={value.get('population')} />
-// 					);
-// 				});
-// 	}	
-// });
-
-
-
-
-
 
 
 
