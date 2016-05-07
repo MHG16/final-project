785e8191812dbca2a0201557e33a9f627ce9c2b8
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-07 14:47:11 -0500

    Working on rendering one planet's data on PlanetPage

--- public/scripts/components/PlanetData.js
+++ public/scripts/components/PlanetData.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import planetcollection from '../collections/PlanetCollection.js';
+import Planet from '../../components/Planet.js';
 
-//component to render a picture and all data on a particular planet 
-//this is displayed on PlanetPage
+/*component to render a picture and all data on the particular planet that was clicked
+on the HomeGalaxy page.  This is displayed on PlanetPage */
+
+
 export default React.createClass({
-	render: function() {
-		return (
-					<h1>This is my Planet Data</h1>
+	
+getInitialState: function(){
+		console.log('getInitialState');
+		return{planetcollection: planetcollection};
+	},
+	componentDidMount: function(){
+		console.log('I mounted Planet!');
+		this.state.planetcollection.on('change update', () => {
+			this.setState({planetcollection: planetcollection});
+			console.log('testing');
+		});
+		this.state.planetcollection.fetch();
+	
+	},
+
+	render: function(){
+			return(
+				<section>
+					<h1>This is where my planet data will go</h1>
+					<Planet name={value.get('name')} />
+				</section> 
 				);
-	}
+	}	
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
