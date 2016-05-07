9ee27058eb5400c4c9e0079023d86fd2d33c44eb
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-07 13:54:07 -0500

    importing PlanetData and Blog components into PlanetPage.js

---
+++ public/scripts/components/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+	render: function() {
+		return (
+			<section>
+				<h1>This is my Blog Component </h1>
+			</section>
+		);
+	}
+});
--- public/scripts/components/pages/PlanetPage.js
+++ public/scripts/components/pages/PlanetPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import planetdata from './PlanetData';
+import blog from './blog.js';
 
 export default React.createClass({
 	render: function() {
 		return (
 			<section>
 				<h1>This is my Planet Page</h1>
+				<planetdata />
+				<blog />
 			</section>
 		);
 	}
