96ef200a15259946a8eadf6147ee248e13dd6964
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-13 14:48:34 -0500

    working on how to position planets on map will need to pass in two coordinates for each in seed data: top, left.  This will require adding two properties to my Planet table: top, left.

--- public/scripts/components/Planet.js
+++ public/scripts/components/Planet.js
@@ -7,8 +7,13 @@ import {Link} from 'react-router';
 //want to use params here so I can pass the planet id to the Planet Page   
 export default React.createClass({
 	render: function() {
+		let style = {
+			position: 'absolute',
+			top: '20%',
+			left: '20%'
+		};
 		return (
-				<section>
+				<section style={style}>
 					<Link to={`/planet/${this.props.id}/planetpage`}>{this.props.name}</Link>
 				</section>
 				);
