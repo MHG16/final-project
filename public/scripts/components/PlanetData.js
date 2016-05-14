48b9495c9a3859ba5cb6f30ab8555786198ec88a
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-14 17:56:19 -0500

    now have planet data in rows

--- public/scripts/components/PlanetData.js
+++ public/scripts/components/PlanetData.js
@@ -14,14 +14,16 @@ export default React.createClass({
 							<img src={this.props.image} width='95%' height='auto' className='planetpic'/>
 						</div>
 						<div className='four columns'>
-							<h2 className='category'>Climate:</h2><h3 className='data'>{this.props.climate}</h3>
-							<h2 className='category'>Terrain:</h2><h3 className='data'>{this.props.terrain}</h3>
-							<h2 className='category'>Surface Water:</h2><h3 className='data'>{this.props.surface_water}</h3>
-							<h2 className='category'>Diameter:</h2><h3 className='data'>{this.props.diameter}</h3>
-							<h2 className='category'>Population:</h2><h3 className='data'>{this.props.population}</h3>
-							<h2 className='category'>Gravity:</h2><h3 className='data'>{this.props.gravity}</h3>
-							<h2 className='category'>Orbital Period:</h2><h3 className='data'>{this.props.orbital_period}</h3>
-							<h2 className='category'>Rotation Period:</h2><h3 className='data'> {this.props.rotation_period}</h3>
+							<ul>
+								<li><h2 className='category'>Climate:</h2><h3 className='data'>{this.props.climate}</h3></li>
+								<li><h2 className='category'>Terrain:</h2><h3 className='data'>{this.props.terrain}</h3></li>
+								<li><h2 className='category'>Surface Water:</h2><h3 className='data'>{this.props.surface_water}</h3></li>
+								<li><h2 className='category'>Diameter:</h2><h3 className='data'>{this.props.diameter}</h3></li>
+								<li><h2 className='category'>Population:</h2><h3 className='data'>{this.props.population}</h3></li>
+								<li><h2 className='category'>Gravity:</h2><h3 className='data'>{this.props.gravity}</h3></li>
+								<li><h2 className='category'>Orbital Period:</h2><h3 className='data'>{this.props.orbital_period}</h3></li>
+								<li><h2 className='category'>Rotation Period:</h2><h3 className='data'> {this.props.rotation_period}</h3></li>
+							</ul>	
 						</div>
 					</div>
 				</section> 
