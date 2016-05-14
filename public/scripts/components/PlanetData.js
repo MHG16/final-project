460a44c52ca9f8d4f85f616082a8939ca909b634
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-14 17:07:40 -0500

    using skeleton to have 2 columns on planet page

--- public/scripts/components/PlanetData.js
+++ public/scripts/components/PlanetData.js
@@ -8,16 +8,22 @@ export default React.createClass({
 	render: function(){
 			return(
 				<section>
-					<p className='data'>{this.props.name}</p>
-					<img src={this.props.image} className='planetpic'/> 
-					<h2>Climate:{this.props.climate}</h2>
-					<h2>Terrain:{this.props.terrain}</h2>
-					<h2>Surface Water:{this.props.surface_water}</h2>
-					<h2>Diameter:{this.props.diameter}</h2>
-					<h2>Population:{this.props.population}</h2>
-					<h2>Gravity:{this.props.gravity}</h2>
-					<h2>Orbital Period:{this.props.orbital_period}</h2>
-					<h2>Rotation Period:{this.props.rotation_period}</h2>
+					<div className='row'>
+						<div className='offset-by-one four columns'>
+							<h1 className='planetname'>{this.props.name}</h1>
+							<img src={this.props.image} className='planetpic'/>
+						</div>
+						<div className='six columns'>
+							<h2>Climate:{this.props.climate}</h2>
+							<h2>Terrain:{this.props.terrain}</h2>
+							<h2>Surface Water:{this.props.surface_water}</h2>
+							<h2>Diameter:{this.props.diameter}</h2>
+							<h2>Population:{this.props.population}</h2>
+							<h2>Gravity:{this.props.gravity}</h2>
+							<h2>Orbital Period:{this.props.orbital_period}</h2>
+							<h2>Rotation Period:{this.props.rotation_period}</h2>
+						</div>
+					</div>
 				</section> 
 				);
 	}	
