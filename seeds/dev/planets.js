f5226526e4340d75fb973f41fe84a8ea75bdb342
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-15 11:28:20 -0500

    positioned first 7 planets on map

--- seeds/dev/planets.js
+++ seeds/dev/planets.js
@@ -4,25 +4,25 @@ exports.seed = function(knex, Promise) {
       return Promise.join(
         // Inserts seed entries
         knex('planets').insert({id: 1, planetName: 'Tatooine', image: '/images/Planet_Images/Tatooine.png',
-          top: 20, left: 20, createdAt: new Date()}),
+          top: 138, left: 50, createdAt: new Date()}),
 
         knex('planets').insert({id: 2, planetName: 'Alderaan', image: '/images/Planet_Images/Alderaan.jpg',
-          top: 20, left: 20, createdAt: new Date()}),
+          top: 80, left: 27, createdAt: new Date()}),
         
         knex('planets').insert({id: 3, planetName: 'Yavin', image: '/images/Planet_Images/Yavin.png',
-          top: 20, left: 20, createdAt: new Date()}),
+          top: 45, left: 45, createdAt: new Date()}),
         
         knex('planets').insert({id: 4, planetName: 'Hoth', image: '/images/Planet_Images/Hoth.jpg',
-          top: 20, left: 20, createdAt: new Date()}),
+          top: 153, left: 19, createdAt: new Date()}),
         
         knex('planets').insert({id: 5, planetName: 'Dagobah', image: '/images/Planet_Images/Dagobah.jpg',
-          top: 20, left: 20, createdAt: new Date()}),
+          top: 159, left: 29, createdAt: new Date()}),
         
         knex('planets').insert({id: 6, planetName: 'Bespin', image: '/images/Planet_Images/Bespin.png',
-          top: 20, left:20, createdAt: new Date()}),
+          top: 158, left:19, createdAt: new Date()}),
         
         knex('planets').insert({id: 7, planetName: 'Endor', image: '/images/Planet_Images/Endor.png',
-          top: 20, left:20, createdAt: new Date()}),
+          top: 140, left:10, createdAt: new Date()}),
         
         knex('planets').insert({id: 8, planetName: 'Naboo', image: '/images/Planet_Images/Naboo.png',
           top: 20, left:20, createdAt: new Date()}),
