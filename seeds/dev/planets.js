0c0d533cbb68e80adca3afab90f54c85030a16d1
Martin Grossmann <mhgrossmann@yahoo.com>
2016-05-16 15:05:45 -0500

    6 planets in position

--- seeds/dev/planets.js
+++ seeds/dev/planets.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
       return Promise.join(
         // Inserts seed entries
         knex('planets').insert({id: 1, planetName: 'Tatooine', image: '/images/Planet_Images/Tatooine.png',
-          top: 50, left: 50, createdAt: new Date()}),
+          top: 73, left: 73, createdAt: new Date()}),
 
         knex('planets').insert({id: 2, planetName: 'Alderaan', image: '/images/Planet_Images/Alderaan.jpg',
           top: 20, left: 20, createdAt: new Date()}),
@@ -13,22 +13,22 @@ exports.seed = function(knex, Promise) {
           top: 20, left: 20, createdAt: new Date()}),
         
         knex('planets').insert({id: 4, planetName: 'Hoth', image: '/images/Planet_Images/Hoth.jpg',
-          top: 20, left: 20, createdAt: new Date()}),
+          top: 83, left: 28, createdAt: new Date()}),
         
         knex('planets').insert({id: 5, planetName: 'Dagobah', image: '/images/Planet_Images/Dagobah.jpg',
           top: 20, left: 20, createdAt: new Date()}),
         
         knex('planets').insert({id: 6, planetName: 'Bespin', image: '/images/Planet_Images/Bespin.png',
-          top: 20, left:20, createdAt: new Date()}),
+          top: 81, left:29, createdAt: new Date()}),
         
         knex('planets').insert({id: 7, planetName: 'Endor', image: '/images/Planet_Images/Endor.png',
-          top: 20, left:20, createdAt: new Date()}),
+          top: 71, left:11, createdAt: new Date()}),
         
         knex('planets').insert({id: 8, planetName: 'Naboo', image: '/images/Planet_Images/Naboo.png',
-          top: 20, left:20, createdAt: new Date()}),
+          top: 73, left:54, createdAt: new Date()}),
         
         knex('planets').insert({id: 9, planetName: 'Coruscant', image: '/images/Planet_Images/Coruscant.png',
-          top: 20, left:20, createdAt: new Date()}),
+          top: 35, left:34, createdAt: new Date()}),
         
         knex('planets').insert({id: 61, planetName: 'Jakku', image: '/images/Planet_Images/Jakku.jpg',
           top: 20, left:20, createdAt: new Date() })
